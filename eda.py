6978c88b5c1ca641f9b0c1617e948edf7696408c
ucaiado <ucaiado@me.com>
2016-06-03 02:20:34 -0300

    implement all remain code task. Now I need to write about it

--- eda.py
+++ eda.py
@@ -54,6 +54,103 @@ def features_boxplot(all_data, samples, indices):
     return ax
 
 
+def cluster_results(reduced_data, preds, centers, pca_samples):
+    '''
+    Visualizes the PCA-reduced cluster data in two dimensions
+    Adds cues for cluster centers and student-selected sample data
+    :param reduced_data: pandas dataframe. the dataset transformed and cleaned
+    :param preds: numpy array. teh cluster classification of each datapoint
+    :param centers: numpy array. the center of the clusters
+    :param pca_samples: numpy array. the sample choosen
+    '''
+
+    predictions = pd.DataFrame(preds, columns=['Cluster'])
+    plot_data = pd.concat([predictions, reduced_data], axis=1)
+
+    # Generate the cluster plot
+    fig, ax = plt.subplots(figsize=(14, 8))
+
+    # Color map
+    cmap = sns.color_palette('Set2', 11)
+
+    # Color the points based on assigned cluster
+    for i, cluster in plot_data.groupby('Cluster'):
+        cluster.plot(ax=ax, kind='scatter', x='Dimension 1', y='Dimension 2',
+                     color=cmap[i],
+                     label='Cluster %i' % (i),
+                     s=30)
+
+    # Plot centers with indicators
+    for i, c in enumerate(centers):
+        ax.scatter(x=c[0], y=c[1], color='white', edgecolors='black',
+                   alpha=1, linewidth=2, marker='o', s=200)
+        ax.scatter(x=c[0], y=c[1], marker='$%d$' % (i), alpha=1, s=100)
+
+    # Plot transformed sample points
+    ax.scatter(x=pca_samples[:, 0], y=pca_samples[:, 1],
+               s=150, linewidth=4, color='black', marker='x')
+
+    # Set plot title
+    s_title = 'Cluster Learning on PCA-Reduced Data - Centroids Marked by'
+    s_title += ' Number\nTransformed Sample Data Marked by Black Cross\n'
+    ax.set_title(s_title, fontsize=16)
+
+
+def channel_results(reduced_data, outliers, pca_samples):
+    '''
+    Visualizes the PCA-reduced cluster data in two dimensions using the full
+    dataset Data is labeled by "Channel" and cues added for student-selected
+    sample data
+    :param reduced_data: pandas dataframe. the dataset transformed and cleaned
+    :param outliers: list. the datapoint considered outliers
+    :param pca_samples: numpy array. the sample choosen
+    '''
+    # Check that the dataset is loadable
+    try:
+        full_data = pd.read_csv('customers.csv')
+    except:
+        print 'Dataset could not be loaded. Is the file missing?'
+        return False
+
+    # Create the Channel DataFrame
+    channel = pd.DataFrame(full_data['Channel'], columns=['Channel'])
+    channel = channel.drop(channel.index[outliers]).reset_index(drop=True)
+    labeled = pd.concat([reduced_data, channel], axis=1)
+
+    # Generate the cluster plot
+    fig, ax = plt.subplots(figsize=(14, 8))
+
+    # Color map
+    # cmap = cm.get_cmap('gist_rainbow')
+    cmap = sns.color_palette('Set2', 11)
+
+    # Color the points based on assigned Channel
+    labels = ['Hotel/Restaurant/Cafe', 'Retailer']
+    grouped = labeled.groupby('Channel')
+    for i, channel in grouped:
+        channel.plot(ax=ax, kind='scatter', x='Dimension 1', y='Dimension 2',
+                     color=cmap[i],
+                     label=labels[i-1],
+                     s=30)
+
+    # Plot transformed sample points
+    for i, sample in enumerate(pca_samples):
+        ax.scatter(x=sample[0], y=sample[1], s=200,
+                   linewidth=3,
+                   color='black',
+                   marker='o',
+                   facecolors='none')
+        ax.scatter(x=sample[0]+0.25, y=sample[1]+0.3,
+                   marker='$%d$' % (i),
+                   alpha=1,
+                   s=125)
+
+    # Set plot title
+    s_title = 'PCA-Reduced Data Labeled by "Channel"\nTransformed Sample Data'
+    s_title += ' Circled'
+    ax.set_title(s_title)
+
+
 def pca_results(good_data, pca):
     '''
     Create a DataFrame of the PCA results. Includes dimension feature weights
